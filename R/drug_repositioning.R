e8b87ac9cc43b5a017cdf88bf5236215ed8fa175
Thomaz Luscher Dias <luxeredias@gmail.com>
2020-02-06 17:14:15 -0300

    code cleaning and organization

--- R/drug_repositioning.R
+++ R/drug_repositioning.R
@@ -2,16 +2,17 @@
 #to perform drug repositioning analysis for Psychiatric and Neurological
 #disorder
 
-#libraries to load
+#####libraries to load####
 library(igraph)
 library(readr)
 library(dplyr)
 options(stringsAsFactors = F)
 
+####set working directory####
 dir <- "~/Área de Trabalho/GitHub_projects/PND_drug_repo/"
 setwd(dir)
 
-#import dis_FNG_FNDrugs file
+####Raw files - import dis_FNG_FNDrugs file####
 workingdir <- "data/wdd_files/"
 df <- list.files(path = paste0(workingdir),pattern = ".csv",full.names = T) %>%
   lapply(read_csv) %>% 
@@ -22,7 +23,7 @@ df <- df[!duplicated(df[,c(3,6)]),]
 df <- df %>%
   filter((Confidence >=50 | Confidence == -1) & Documents > 1)
 
-#gene-PND and drug-PND networks
+####Fig 1A: get gene-PND and drug-PND networks####
 df_drugs <- df[df$`Source type`=="DRUG",]
 wdd_all_drugs <- unique(df_drugs$`Source Display Name`)
 save(wdd_all_drugs,file = "data/01_wdd_all_drugs.RData")
@@ -31,7 +32,8 @@ df_genes <- df[df$`Source type`=="GENE",]
 wdd_all_genes <- unique(df_genes$`Source Display Name`)
 save(wdd_all_genes,file = "data/02_wdd_all_genes.RData")
 
-#get exclusive genes
+####Fig 1B: get exclusive coexpressed genes####
+#make nodes and edges table to calculate degree
 nodes <- data.frame(Id=c(unique(df_genes$`Source Display Name`),
                          unique(df_genes$`Target Display Name`)),
                     Class=c(rep("GENE",1588),rep("CONDITION",9)))
@@ -44,6 +46,8 @@ graph <- graph_from_data_frame(d = edges,directed = F)
 degree <- degree(graph)
 nodes$degree <- degree(graph)
 
+#save files to be viwed in gephi and analyzed with other scripts
+#(Fig. 2 and Fig. S1)
 write.csv(edges,file="data/03_gene_PNDs_edges.csv",quote = T,row.names = F)
 write.csv(nodes,file="data/03_gene_PNDs_nodes.csv",quote = T,row.names = F)
 
@@ -54,7 +58,7 @@ exclusive_genes_edges <- edges %>%
 
 save(exclusive_genes,file = "data/04_exclusive_genes.RData")
 
-#Gandal info (genes coexpressed in PNDs)
+#Load Gandal coexpression information (genes coexpressed in PNDs)
 gandal <- read.csv("data/gandal_2018a.csv",dec = ",")
 gandal_genes <- gandal %>%
   filter(Module.name!="CD0") %>%
@@ -71,22 +75,23 @@ exclusive_genes_coex_edges <- df_genes %>%
   select(`Source Display Name`,`Target Display Name`,Confidence,`Document IDs`) %>%
   rename(Source=`Source Display Name`,Target=`Target Display Name`)
 
+#Save file with all exclusive coexpressed genes in PNDs (Table S1)
 save(exclusive_genes_coex_edges,file="data/05_exclusive_genes_coex_edges.RData")
 write.csv(exclusive_genes_coex_edges,file = "data/Table_S1_exclusive_coexpressed_genes.csv",row.names = F)
 
-#####GET ALL WDD SEARCHES WITH GENES AND MAKE DRUG_GENE DATAFRAME###############
-genes_files <- paste0("/home/thomaz/first_neighbor_genes_all/",exclusive_genes_coex,"_50perc_2doc_50y.csv")
+####Fig. 1C:Get WDD searches with exclusive coexpresed genes####
+genes_files <- paste0("data/first_neighbor_genes_all/",exclusive_genes_coex,"_50perc_2doc_50y.csv")
 
 #drug-gene relations from raw source
-df_drug_genes <- list.files(path = "/home/thomaz/first_neighbor_genes_all",pattern = ".csv",full.names = T) %>%
+df_drug_genes <- list.files(path = "data/first_neighbor_genes_all",pattern = ".csv",full.names = T) %>%
   .[. %in% genes_files] %>%
   lapply(read_csv) %>% 
   bind_rows
-  
+
 all_drug_gene <- df_drug_genes %>%
   filter(Confidence >= 50 & Documents >1 | Documents==-1) %>%
   filter(`Source type`=="DRUG" & `Target type`=="GENE") %>%
-  select(`Source Display Name`,`Target Display Name`) %>%
+  select(`Source Display Name`,`Target Display Name`,Confidence,`Document IDs`) %>%
   rename(Source=`Source Display Name`,Target=`Target Display Name`)
 
 #drugs that affect exclusive genes coex
@@ -98,7 +103,8 @@ length(unique(exclusive_genes_drugs$Source))
 write.table(exclusive_genes_coex,file="data/05_exclusive_genes_coex.txt",quote = F,row.names = F,col.names = F)
 save(exclusive_genes_drugs,file="data/06_exclusive_coex_genes_drugs.RData")  
 
-#remove known PND drugs
+####Fig. 1D: remove known PND drugs and drugs that affect > 1 target####
+#remove known drugs
 exclusive_genes_drugs <- exclusive_genes_drugs %>%
   filter(!Source %in% unique(df_drugs$`Source Display Name`))
 
@@ -115,72 +121,36 @@ drug_nodes <- data.frame(Id=c(unique(exclusive_genes_drugs$Source),
                          )
 
 #make igraph object of the drug-gene network
-drug_graph <- graph_from_data_frame(d = exclusive_genes_drugs,directed = F)
+drug_graph <- graph_from_data_frame(d = exclusive_genes_drugs[,-c(3,4)],directed = F)
 
 #calculate drug-gene network degree
 drug_nodes$degree <- degree(drug_graph)
 
-#keep only drugs that affect one gene (among all genes in the original gene-PND network)
+#keep only drugs that affect one gene
 final_drugs <- drug_nodes$Id[drug_nodes$Class=="DRUG" & drug_nodes$degree==1]
-
 length(unique(final_drugs))
 
-#create object to curate each drug-gene-disease relationship
-drug_gene_disease_curate <- exclusive_genes_drugs %>%
+####Fig. 1E: create table for drug prioritization and selection####
+#create table with all WDD gene-disease references
+gene_disease_references <- df_genes %>%
+  select(`Source Display Name`,`Target Display Name`,`Document IDs`) %>%
+  rename(Gene=`Source Display Name`,PND=`Target Display Name`,gene_PND_ref=`Document IDs`)
+
+#create table with all WDD drug-gene references
+drug_gene_references <- exclusive_genes_drugs %>%
   filter(Source %in% final_drugs) %>%
-  select(Source,Target)
+  select(Source,Target,`Document IDs`) %>%
+  rename(Drug=Source,Gene=Target,drug_gene_ref=`Document IDs`)
 
-drug_gene_disease_curate <- merge(drug_gene_disease_curate,gene_disease_edges,
-                                  by.x="Target",by.y="Source") %>%
-  select(Source,Target,Target.y)
-
-colnames(drug_gene_disease_curate) <- c("DRUG","GENE","DISEASE")
-
-final_genes <- unique(drug_gene_disease_curate$GENE)
-length(unique(final_genes))
-
-#make file to be used in the drug prioritization steps
-exclusive_genes_coex_refereces <- df_genes %>%
-  filter(`Source Display Name` %in% exclusive_genes_coex) %>%
-  select(`Source Display Name`,`Target Display Name`,Confidence,`Document IDs`) %>%
-  rename(Source=`Source Display Name`,Target=`Target Display Name`)
-
-write.csv(exclusive_genes_coex_refereces,file="data/Table_S1_exclusive_genes_references.csv",quote = T,row.names = F)
-
-final_gene_disease_references <- df_genes %>%
-  filter(`Source Display Name` %in% final_genes) %>%
-  select(`Source Display Name`,`Target Display Name`,Confidence,`Document IDs`) %>%
-  rename(Source=`Source Display Name`,Target=`Target Display Name`)
-
-#####GET ALL WDD SEARCHES WITH GENES AND MAKE DRUG_GENE DATAFRAME###############
-genes_files <- paste0("/home/thomaz/first_neighbor_genes_all/",exclusive_genes_coex,"_50perc_2doc_50y.csv")
-
-#drug-gene relations from raw source
-df_drug_genes <- list.files(path = "~/first_neighbor_genes_all",pattern = ".csv",full.names = T) %>%
-  .[. %in% genes_files] %>%
-  lapply(read_csv) %>% 
-  bind_rows
-
-all_drug_gene <- df_drug_genes %>%
-  filter(Confidence >= 50 & Documents >1 | Documents==-1) %>%
-  filter(`Source type`=="DRUG" & `Target type`=="GENE") %>%
-  select(`Source Display Name`,`Target Display Name`,Confidence,`Document IDs`) %>%
-  rename(Source=`Source Display Name`,Target=`Target Display Name`)
-
-################################################################################
-
-final_drug_gene_references <- all_drug_gene %>%
-  filter(Source %in% final_drugs & Target %in% final_genes)
-
-final_drug_gene_disease_references <- merge(final_drug_gene_references,final_gene_disease_references,
-                                            by.x="Target",by.y="Source")
-
-final_drug_gene_disease_references <- final_drug_gene_disease_references %>%
-  select(Source,Target,Target.y,`Document IDs.y`,`Document IDs.x`) %>%
-  rename(drug=Source,gene=Target,PND=Target.y,
-         gene_dis_ref=`Document IDs.y`,
-         drug_gene_ref=`Document IDs.x`) %>%
+#create drug-gene-disease table with WDD references for prioritization
+drug_gene_disease_reference <- merge(drug_gene_references,gene_disease_references,
+                                  by.x="Gene",by.y="Gene") %>%
+  select(Drug,Gene,PND,gene_PND_ref,drug_gene_ref) %>%
   arrange(PND)
 
-write.csv(final_drug_gene_disease_references,file="data/Table_S2_potential_novel_PND_drugs.csv",quote = T,row.names = F)
-save(final_drug_gene_disease_references,file="data/07_drug_gene_disease_final.RData")
+final_genes <- unique(drug_gene_disease_curate$Gene)
+length(unique(final_genes))
+
+#save file to be used in the drug prioritization steps (Table S2)
+write.csv(drug_gene_disease_reference,file="data/Table_S2_potential_novel_PND_drugs.csv",quote = T,row.names = F)
+save(drug_gene_disease_reference,file="data/07_drug_gene_disease_final.RData")
